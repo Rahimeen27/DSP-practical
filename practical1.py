f31426333ed1232ae20e1597b0d105979eff9a1e
Rahimeen Sheikh <rahemeenanjum17@gmail.com>
2026-09-04 19:56:54 +0530

    Add files via upload

---
+++ practical1.py
@@ -0,0 +1,48 @@
+print(" Student Data System)
+
+# Input section (user provides data)
+
+#str name= input("Enter student name: ")
+
+roll_no= int(input("Enter roll number: ")) # int
+
+age=int(input("Enter age: "))
+
+marks = float(input("Enter marks: "))
+
+#float
+
+is_pass marks >= 40
+
+#int
+
+#bool (logical expression)
+percentage (marks/100)* 100
+
+#Output section
+
+print("\n Student Details")
+
+print("Name:", name)
+
+print("Roll Number:", roll_no)
+
+print("Age:", age)
+
+print("Marks:", marks)
+
+print("Percentage:", percentage)
+
+print("Result:", "Pass" if is_pass else "Fail")
+
+#Display data types (to understand building blocks)
+
+print("\n Data Types Used")
+
+print("Type of name:", type(name))
+
+print("Type of roll_no:", type(roll_no))
+
+print("Type of age", type(age)) print("Type of marks:", type(marks))
+
+print("Type of is_pass:", type(is_pass))
